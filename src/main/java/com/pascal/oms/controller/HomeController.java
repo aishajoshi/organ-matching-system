3037e42da099f317e27dcc758b6c965680cd2998
aishajoshi <137846404+aishajoshi@users.noreply.github.com>
2025-06-13 14:01:25 +0545

    few fixes

--- src/main/java/com/pascal/oms/controller/HomeController.java
+++ src/main/java/com/pascal/oms/controller/HomeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
-public class HomeController {
+public class   HomeController {
     private final ParkingTicketManagementService service = ParkingTicketManagementService.getInstance();
 
 
